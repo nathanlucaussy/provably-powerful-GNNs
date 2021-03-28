67cd5b43147b9c22316080308729f882a0da00c3
JUST0M <tommasonuk@yahoo.co.uk>
2021-03-28 17:32:43 +0100

    Comment out bad line for now

--- model_utils.py
+++ model_utils.py
@@ -2,7 +2,7 @@ import random
 import torch_geometric as tg
 import torch
 from random import shuffle
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 lr_parameters = [0.00005, 0.0001, 0.0005, 0.001]
 decay_parameters = [0.5, 1]
