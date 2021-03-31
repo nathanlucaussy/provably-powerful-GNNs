499fad00b6ea308902389b44f6043e28e9688866
JUST0M <tommasonuk@yahoo.co.uk>
2021-03-31 12:18:14 +0100

    Move S2VGraph to GIN_wrapper

--- model_wrappers/GIN_wrapper.py
+++ model_wrappers/GIN_wrapper.py
@@ -39,3 +39,23 @@ class GINWrapper(ModelWrapper):
     
     def run(self):
         return 0.5 # TODO: Obviously needs changing!
+    
+class S2VGraph(object):
+    def __init__(self, g, label, node_tags=None, node_features=None):
+        '''
+            g: a networkx graph
+            label: an integer graph label
+            node_tags: a list of integer node tags
+            node_features: a torch float tensor, one-hot representation of the tag that is used as input to neural nets
+            edge_mat: a torch long tensor, contain edge list, will be used to create torch sparse tensor
+            neighbors: list of neighbors (without self-loop)
+        '''
+        self.label = label
+        self.g = g
+        self.node_tags = node_tags
+        self.neighbors = []
+        self.node_features = 0
+        self.edge_mat = 0
+
+        self.max_neighbor = 0
+    
