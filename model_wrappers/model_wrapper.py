bea3b6a85fa9794d08aca7da5f50247ab2197e75
JUST0M <tommasonuk@yahoo.co.uk>
2021-04-01 11:38:37 +0100

    Add tools to download and import code for other models

--- model_wrappers/model_wrapper.py
+++ model_wrappers/model_wrapper.py
@@ -1,12 +1,18 @@
 from torch_geometric.datasets import TUDataset
 import os
 import sys
+from urllib.request import urlopen
+from zipfile import ZipFile
+import importlib.util
 
 class ModelWrapper:
     
     use_node_attr = True
-    data_dir = os.path.join(os.path.dirname(__file__), '../tg_datasets')
+    curr_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(curr_dir, '../tg_datasets')
+    models_path = os.path.join(curr_dir, '../models/other')
     config = {} # Default config to be properly defined by child class
+    model_dir = None
     
     def __init__(self, dataset, config):
         self.data = TUDataset(self.data_dir,
@@ -26,4 +32,24 @@ class ModelWrapper:
         
     def run(self):
         raise NotImplementedError()
+        
+    def download_repo(self, url, model_name):
+        self.model_dir = os.path.join(self.models_path, model_name)
+        self.model_name = model_name
+        if os.path.exists(self.model_dir):
+            print(f'Directory for model {model_name} already exists, not downloading')
+        else:
+            print(f'Downloading model: {model_name}')
+            zip_resp = urlopen(url)
+            with open('temp.zip', 'wb') as temp_zip:
+                temp_zip.write(zip_resp.read())
+            zip_file = ZipFile('temp.zip')
+            zip_file.extractall(path = self.model_dir)
+            zip_file.close()
+            os.remove('temp.zip')
+        
+
+    def import_model_dir(self):
+        # Bit hacky but ah well
+        exec(f'from models.other import {self.model_name}')
         
